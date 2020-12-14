6f0c1ee54b757552df07f1b07a67be34d917375a
LeoIta <leonardolorusso83@gmail.com>
2020-12-14 09:20:44 +0100

    my solution

--- basics-assignment-2-databinding/src/app/app.component.ts
+++ basics-assignment-2-databinding/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   username = '';
+  intro = "who are you?";
+  allowReset=false;
+  
+  getIntro(){
+  this.intro = (this.username =='') ? "who are you?": "Hello ";
+  this.allowReset=(this.username =='') ? false:true;
+  return this.intro; 
+  }
+  onClickReset(){
+    this.username='';
+  }
+
 }
+
